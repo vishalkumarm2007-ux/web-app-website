84d6e48a01e7f4377cbe50e634b0ca5bfe2e3aaf
vishalkumarm2007-ux <vishalkumarm2007@gmail.com>
2026-09-24 23:44:37 +0530

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,18 @@
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>IPL Ticket Booking</h2>
+
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/matches">Matches</Link>
+        <Link to="/booking">Book Ticket</Link>
+        <Link to="/history">Booking History</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
